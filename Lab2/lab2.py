d7f39902fb2f774986459c68bd3f13ec0dff3a35
yoni13-meet <yoni13@meet.mit.edu>
2013-11-10 21:49:56 +0200

    Update lab2.py

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -7,13 +7,3 @@ def divisors(n):
 		if n%(i+1) == 0:
      			print i+1
 divisors(n)
-
-
-#execise 2
-
-x=raw_input('Please insert a word')
-y = x[::-1]
-if x==y:
-    print("Is a palindrome")
-else:
-    print("Is not a palindrome")
