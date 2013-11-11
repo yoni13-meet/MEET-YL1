791840698fce9ae6b749a08510b938429fb89319
yoni13-meet <yoni13@meet.mit.edu>
2013-11-11 16:31:11 +0200

    Update lab2.py

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -20,6 +20,50 @@ else:
     
 #exersice 3
 
+import random
+def flip():
+    n = raw_input("insert n for next")
+    if n == "n":
+        a = random.randint(1,2)          
+        if a == 1:
+            print "T"
+        else:
+            print "H"
+
+
+def chance():
+    to_flip_heads = raw_input("what is the probability for heads")
+    a = random.randint(1, 100)
+    if a < to_flip_heads:
+        print "heads"
+    else:
+        print "tails"
+
+def decide_flips():
+    t_counter=0
+    h_counter=0
+    n = int (raw_input("how many times to flip the coin?"))
+    for i in xrange(n):
+        a = random.randint(1,2)          
+        if a == 1:
+            print "T"
+            t_counter +=1
+        else:
+            print "H"
+            h_counter +=1
+            
+    print str(t_counter) + "times tails"
+    print str(h_counter) + "times heads"
+
+#exercise 4
+
+
+
+
+
+
+
+
 
 
 
